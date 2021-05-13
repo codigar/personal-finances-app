f767c379c52279d966dc786729b1b0e05735467f
Códigar <codigarprogramacao@gmail.com>
2021-05-13 17:51:03 -0300

    launch options is done

--- js/new-launch.js
+++ js/new-launch.js
@@ -1,17 +1,63 @@
 const incomeExpense = document.querySelector('#income-expense');
+const form = document.querySelector('#form');
 const income = document.querySelector('#income');
 const expense = document.querySelector('#expense');
-const incomeForm = document.querySelector('#income-form');
+const launchForm = document.querySelector('#launch-form');
+const launchType = document.querySelector('#launchtype');
+const btnLaunchForm = document.querySelector('#btn-launch-form');
 const btnCancel = document.querySelector('#btn-cancel');
+let launches = JSON.parse(localStorage.getItem('launches')) || [];
+let launchId = Number(localStorage.getItem('id')) || 1;
 
 income.addEventListener('click', () => {
-    incomeExpense.style.justifyContent = 'center';
     expense.style.display = 'none';
-    incomeForm.style.display = 'flex';
+    incomeExpense.style.justifyContent = 'center';
+    launchForm.style.display = 'flex';
+    launchType.setAttribute('value', 'income');
+});
+
+expense.addEventListener('click', () => {
+    income.style.display = 'none';
+    incomeExpense.style.justifyContent = 'center';
+    launchForm.style.display = 'flex';
+    launchType.setAttribute('value', 'expense');
+    btnLaunchForm.style.backgroundColor = 'red';
 });
 
 btnCancel.addEventListener('click', () => {
-    incomeExpense.style.justifyContent = 'space-between';
+    income.style.display = 'flex';
     expense.style.display = 'flex';
-    incomeForm.style.display = 'none';
+    incomeExpense.style.justifyContent = 'space-between';
+    launchForm.style.display = 'none';
+    btnLaunchForm.style.backgroundColor = '#0059ff';
 });
+
+const sendLaunchObject = launchObject => {
+    launches.push(launchObject);
+    localStorage.setItem('launches', JSON.stringify(launches));
+    localStorage.setItem('id', launchId);
+    location = '/new-launch';
+}
+
+const createLaunchObject = (launchType, description, amount) => {
+    const launchObject = Object.create(null);
+    launchObject.id = launchId;
+    launchId++; // increase id value
+    launchObject.launchType = launchType;
+    launchObject.description = description;
+    launchObject.amount = amount;
+
+    sendLaunchObject(launchObject);
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const launchType = e.target.launchtype.value;
+    const description = e.target.description.value;
+    const amount = Number(e.target.amount.value);
+
+    createLaunchObject(launchType, description, amount);
+});
+
+console.log(launches);
